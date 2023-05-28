5b475e6132bbbc2f76deeb2852b230ddfdbdd9fd
sieun0322 <lce322@naver.com>
2023-05-28 18:57:25 +0900

    feat: TextInput styled component

---
+++ app/components/styled/components/TextInput.tsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components/native';
+import {TextInput as Input}  from 'react-native-paper';
+type Props = {
+  width?: number;
+  marginBottom?: number;
+};
+
+const StyledTextInput = styled(Input).attrs(()=>({
+  underlineColor:'transparent',
+}))<Props>`
+    width: ${({ width }) => (width ? `${width}%` : 'auto')};
+    marginBottom: ${({ marginBottom }) => (marginBottom ? `${marginBottom}px` : '8px')};
+ `;
+function TextInput({ ...props }) {
+
+  return (
+      <StyledTextInput {...props} >
+      </StyledTextInput>
+  );
+}
+export default TextInput;
