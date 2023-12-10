e9202e489e69e8d75449a431eb6efa0b8666ac94
sammy <sammyhong0529@google.co.kr>
2023-12-10 21:42:16 +0900

    [LP-94] feat: 계정 관리(수정) 화면 개발

--- app/recoils/user.recoil.ts
+++ app/recoils/user.recoil.ts
@@ -1,8 +1,45 @@
-import {atom} from 'recoil';
+import {atom, selector} from 'recoil';
 import {UserType} from '../types/user.type';
 import {DUMMY_USER} from '../constants/dummy.constant';
+import {PhotoIdentifier} from '@react-native-camera-roll/camera-roll';
 
 export const userState = atom<UserType>({
   key: 'userState',
   default: DUMMY_USER,
+});
+
+export const selectedUserPhotoState = atom<PhotoIdentifier | undefined>({
+  key: 'selectedUserPhotoState',
+  default: undefined,
+});
+
+export const writingUserState = atom<UserType>({
+  key: 'wrtingUserState',
+  default: {
+    userNo: -1,
+    userId: '',
+    userNickName: '게스트',
+    userType: 'none',
+    recentHeroNo: -1,
+    imageURL: '',
+    modifiedImage: undefined,
+  },
+});
+
+export const getCurrentUserPhotoUri = selector({
+  key: 'currentUserPhotoUriState',
+  get: ({get}) => {
+    const user = get(writingUserState);
+
+    if (!user) {
+      return;
+    }
+
+    const modifiedImage = user.modifiedImage;
+    const currentUserPhotoUri = modifiedImage
+      ? modifiedImage.node.image.uri
+      : user.imageURL;
+
+    return currentUserPhotoUri;
+  },
 });
--- app/types/user.type.ts
+++ app/types/user.type.ts
@@ -1,8 +1,11 @@
+import {PhotoIdentifier} from '@react-native-camera-roll/camera-roll';
+
 export type UserType = {
   userNo: number;
   userId: string;
   userNickName: string;
   recentHeroNo: number;
+  imageURL: undefined | string;
   userType: 'general' | 'kakao' | 'apple' | 'none';
-  imageURL: string;
+  modifiedImage?: PhotoIdentifier | undefined;
 };
