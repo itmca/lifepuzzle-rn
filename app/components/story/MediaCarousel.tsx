6265232703daf0d1a79b253bcd8856c08d1957f2
zion <lce322@naver.com>
2025-01-13 23:46:16 +0900

    [LP-348] feat: 상세 사진/영상 높이 고정 (#174)

--- app/components/story/MediaCarousel.tsx
+++ app/components/story/MediaCarousel.tsx
@@ -33,11 +33,6 @@ export const MediaCarousel = ({
   isFocused,
   onScroll,
 }: Props): JSX.Element => {
-  const DeviceWidth = Dimensions.get('window').width;
-
-  const [carouselHeight, setCarouselHeight] = useState(new Animated.Value(200));
-  const [mediaWithSizes, setMediaWithSizes] = useState<MediaItem[]>([]);
-
   const [activeMediaIndexNo, setActiveMediaIndexNo] = useState<number>(
     activeIndex ?? 0,
   );
@@ -53,22 +48,14 @@ export const MediaCarousel = ({
         {type === 'VIDEO' && (
           <VideoPlayer
             videoUrl={mediaUrl}
-            isFocused={isFocused}
             width={carouselWidth}
             activeMediaIndexNo={activeMediaIndexNo}
             setPaginationShown={setIsPaginationShown}
-            onLoad={metadata =>
-              updateVideoHeight(
-                index,
-                metadata.naturalSize.width,
-                metadata.naturalSize.height,
-              )
-            }
           />
         )}
         {type === 'IMAGE' && (
           <Photo
-            resizeMode={height < carouselMaxHeight ? 'center' : 'contain'}
+            resizeMode={'contain'}
             source={{
               uri: mediaUrl,
             }}
@@ -77,81 +64,18 @@ export const MediaCarousel = ({
       </ContentContainer>
     );
   };
-  const animateHeight = async (index: number) => {
-    const newHeight = mediaWithSizes[index]?.height || 200;
-    await Animated.timing(carouselHeight, {
-      toValue: newHeight,
-      duration: 300,
-      useNativeDriver: false,
-    }).start();
-  };
-  const updateVideoHeight = (index: number, width: number, height: number) => {
-    setMediaWithSizes(
-      mediaWithSizes.map(item =>
-        item.index === index
-          ? {...item, height: (height / width) * DeviceWidth}
-          : item,
-      ),
-    );
-  };
-  useEffect(() => {
-    const fetchMediaSizes = async () => {
-      const promises = data.map(
-        (item, index) =>
-          new Promise(resolve => {
-            if (item.type === 'IMAGE') {
-              // 이미지 크기 가져오기
-              Image.getSize(
-                item.url,
-                (width, height) => {
-                  let carousel =
-                    height < carouselMaxHeight
-                      ? {width, height}
-                      : {
-                          width: (width * carouselMaxHeight) / height,
-                          height: carouselMaxHeight,
-                        };
-                  if (carousel.width > DeviceWidth) {
-                    carousel = {
-                      width: DeviceWidth,
-                      height: (DeviceWidth * height) / width,
-                    };
-                  }
-                  resolve({...item, height: carousel.height, index});
-                },
-                error => {
-                  console.error('이미지 로드 에러:', error);
-                  resolve({...item, height: 200, index}); // 기본 높이 설정
-                },
-              );
-            } else if (item.type === 'VIDEO') {
-              // 동영상은 기본 높이 사용 (사용자 정의 가능)
-              resolve({...item, height: 300, index});
-            } else {
-              resolve({...item, height: 300, index});
-            }
-          }),
-      );
-      const results = await Promise.all(promises);
-      setMediaWithSizes(results);
-    };
-    fetchMediaSizes();
-  }, [data]);
-  useEffect(() => {
-    animateHeight(activeMediaIndexNo);
-  }, [mediaWithSizes]);
+
   return (
-    <Animated.View style={{height: carouselHeight}}>
+    <>
       <Carousel
         style={{alignSelf: 'center'}}
         loop={false}
         width={carouselWidth}
-        height={Number(carouselHeight)}
-        data={mediaWithSizes}
+        height={300}
+        data={data}
         defaultIndex={activeMediaIndexNo}
         renderItem={renderItem}
         onSnapToItem={index => {
-          animateHeight(index);
           setActiveMediaIndexNo(index);
           onScroll && onScroll(index);
         }}
@@ -161,6 +85,6 @@ export const MediaCarousel = ({
         activeMediaIndexNo={activeMediaIndexNo}
         mediaCount={data.length}
       />
-    </Animated.View>
+    </>
   );
 };
