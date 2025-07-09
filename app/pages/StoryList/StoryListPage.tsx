9992767ca7cb16b5eda3e486901f7fc7209098a4
zion <lce322@naver.com>
2025-07-09 12:13:13 +0900

    [LP-445] feat: [FE][화면] 사진 목록 스크롤 기능 추가 (#261)
    * feat: scroll 추가
    
    * feat: 지워진 onPressImage 추가
    
    ---------
    
    Co-authored-by: border-line <lord1229@gmail.com>

--- app/pages/StoryList/StoryListPage.tsx
+++ app/pages/StoryList/StoryListPage.tsx
@@ -1,62 +1,67 @@
-import React, {useEffect, useRef} from 'react';
-import {findNodeHandle, FlatList, UIManager, View} from 'react-native';
+// 1. React
+import React, {useEffect, useRef, useState} from 'react';
+
+import {Dimensions, findNodeHandle, UIManager, View} from 'react-native';
+
 import MasonryList from 'react-native-masonry-list';
 import {useRecoilState, useRecoilValue} from 'recoil';
+import {AgeType, GalleryType} from '../../types/photo.type';
+
+import {Head} from '../../components/styled/components/Text';
+
+import {ScreenContainer} from '../../components/styled/container/ScreenContainer.tsx';
+import {
+  ContentContainer,
+  ScrollContentContainer,
+} from '../../components/styled/container/ContentContainer';
+
+import {SCREEN_HEIGHT} from '@gorhom/bottom-sheet';
+
 import {
   ageGroupsState,
   selectedGalleryIndexState,
   selectedTagState,
   tagState,
 } from '../../recoils/photos.recoil';
-import {AgeType, GalleryType} from '../../types/photo.type';
-import {Head} from '../../components/styled/components/Text';
-import {
-  ContentContainer,
-  ScrollContentContainer,
-} from '../../components/styled/container/ContentContainer';
-import {ScreenContainer} from '../../components/styled/container/ScreenContainer.tsx';
+import {isLoggedInState} from '../../recoils/auth.recoil.ts';
 import {useNavigation} from '@react-navigation/native';
 import {BasicNavigationProps} from '../../navigation/types.tsx';
-import {isLoggedInState} from '../../recoils/auth.recoil.ts';
 
 const StoryListPage = () => {
+  const screenWidth = Dimensions.get('window').width;
+  const screenHeight = Dimensions.get('window').height;
+
+  const scrollContainerRef = useRef<any>(null);
+  const itemRefs = useRef<{[key in AgeType]?: View | null}>({});
+
+  const [scrollViewHeight, setScrollViewHeight] = useState(0);
+
   const navigation = useNavigation<BasicNavigationProps>();
+
   const isLoggedIn = useRecoilValue(isLoggedInState);
   const [selectedGalleryIndex, setSelectedGalleryIndex] =
     useRecoilState<number>(selectedGalleryIndexState);
-
   const [tags] = useRecoilState(tagState);
   const [ageGroups] = useRecoilState(ageGroupsState);
   const [selectedTag] = useRecoilState(selectedTagState);
 
-  const flatListRef = useRef<FlatList>(null);
-  const itemRefs = useRef<{[key in AgeType]?: View | null}>({});
+  const ageGroupsArray = Object.entries(ageGroups);
 
-  // 스크롤 이동 (선택된 태그로)
-  useEffect(() => {
-    const targetRef = itemRefs.current[selectedTag.key as AgeType];
-    const listRef = flatListRef.current;
-
-    if (targetRef && listRef) {
-      const nodeHandle = findNodeHandle(targetRef);
-      if (nodeHandle) {
-        UIManager.measureLayout(
-          nodeHandle,
-          findNodeHandle(listRef),
-          () => console.warn(),
-          (_x, y) => {
-            flatListRef.current?.scrollToOffset({offset: y, animated: true});
-          },
-        );
-      }
+  const handleScrollViewLayout = (event: any) => {
+    const {height} = event.nativeEvent.layout;
+    if (height >= scrollViewHeight + SCREEN_HEIGHT) {
+      setScrollViewHeight(height);
     }
-  }, [selectedTag]);
+  };
+
   const moveToStoryDetailPage = (gallery: GalleryType) => {
     const index =
       ageGroups[selectedTag.key as AgeType]?.gallery.find(
         elem => elem.id === gallery.id,
       )?.index ?? 1;
+
     setSelectedGalleryIndex(index - 1);
+
     navigation.push('NoTab', {
       screen: 'StoryViewNavigator',
       params: {
@@ -64,29 +69,67 @@ const StoryListPage = () => {
       },
     });
   };
+  useEffect(() => {
+    const targetRef = itemRefs.current[selectedTag.key as AgeType];
+    const scrollRef = scrollContainerRef.current?.getScrollResponder();
+
+    if (targetRef && scrollRef) {
+      const nodeHandle = findNodeHandle(targetRef);
+      const scrollNodeHandle = findNodeHandle(scrollRef);
+
+      if (nodeHandle && scrollNodeHandle) {
+        UIManager.measureLayout(
+          nodeHandle,
+          scrollNodeHandle,
+          () => console.warn('Failed to measure layout for scroll!'),
+          (_x, y) => {
+            scrollRef.scrollTo({y: y, animated: true});
+          },
+        );
+      }
+    }
+  }, [scrollViewHeight]);
+
   return (
     <ScreenContainer>
-      <ScrollContentContainer paddingVertical={16}>
-        {Object.entries(ageGroups).map(([ageKey, ageGroup]) => {
+      <ScrollContentContainer
+        ref={scrollContainerRef}
+        paddingVertical={16}
+        onLayout={handleScrollViewLayout}
+        scrollEventThrottle={50}>
+        {ageGroupsArray.map(([ageKey, ageGroup], index) => {
+          const isLastAgeGroup = index === ageGroupsArray.length - 1;
+
           return (
-            <ContentContainer gap={0}>
-              <ContentContainer paddingHorizontal={20}>
-                <Head>
-                  {tags.filter(item => item.key === ageKey)[0].label}(
-                  {ageGroups[ageKey as AgeType]?.galleryCount})
-                </Head>
+            <ContentContainer
+              ref={ref => (itemRefs.current[ageKey as AgeType] = ref)}
+              collapsable={false}
+              key={ageKey}
+              minHeight={isLastAgeGroup ? screenHeight : screenHeight / 3}>
+              <ContentContainer gap={0}>
+                <ContentContainer paddingHorizontal={20}>
+                  <Head>
+                    {tags.filter(item => item.key === ageKey)[0]?.label ||
+                      ageKey}{' '}
+                    ({ageGroup.galleryCount || 0})
+                  </Head>
+                </ContentContainer>
+                <MasonryList
+                  images={ageGroup.gallery.map((e: GalleryType) => ({
+                    uri: e.url,
+                    id: e.id,
+                    width: e.width,
+                    height: e.height,
+                  }))}
+                  numColumns={2}
+                  spacing={4}
+                  containerWidth={screenWidth}
+                  imageContainerStyle={{borderRadius: 12}}
+                  onPressImage={(gallery: GalleryType) => {
+                    moveToStoryDetailPage(gallery);
+                  }}
+                />
               </ContentContainer>
-              <MasonryList
-                images={ageGroup.gallery.map((e: GalleryType) => ({
-                  uri: e.url,
-                  id: e.id,
-                }))}
-                spacing={4}
-                imageContainerStyle={{borderRadius: 12}}
-                onPressImage={(gallery: GalleryType) => {
-                  moveToStoryDetailPage(gallery);
-                }}
-              />
             </ContentContainer>
           );
         })}
